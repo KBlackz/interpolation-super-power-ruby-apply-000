b519a1e92c1568b86ebf4907b5b853daf79e6298
blessed-panel-9704 <karris.b@gmail.com>
2020-10-11 21:19:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,6 +1,6 @@
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet' ]
 
   def display_rainbow(colors)
-    colers = 
+    colers = "R:#{colors[1]}, O:#{colors[2]}, Y:#{colors[3]}, G:#{colors[4]}, B:#{colors[5]}, I:#{colors[6]}, V:#{colors[7]}
     
   end
