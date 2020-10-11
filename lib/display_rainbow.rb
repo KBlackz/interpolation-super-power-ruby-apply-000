9d1a7ce8b55abf2081eb4680d56dbd4fc7378e15
blessed-panel-9704 <karris.b@gmail.com>
2020-10-11 20:27:01 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,6 @@
 def display_rainbow(letters)
   letters = ["R", "O", "Y", "G", "B", "I", "V" ]
   puts "the first letter in the alphabet is #{R[red]
-  
+   puts "the first letter in the alphabet is #{O[orange]
+   
   
